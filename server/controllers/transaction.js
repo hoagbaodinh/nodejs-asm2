ad447e46aa91a7f0f94c1a87195a6baa241f6889
hoagbaodinh <46434823+hoagbaodinh@users.noreply.github.com>
2024-02-20 15:10:33 +0700

    Update transaction.js

--- server/controllers/transaction.js
+++ server/controllers/transaction.js
@@ -1,4 +1,4 @@
-const Transaction = require("../models/transaction");
+const Transaction = require("../models/transaction.js");
 
 // Create Transaction
 exports.addTransaction = async (req, res) => {
